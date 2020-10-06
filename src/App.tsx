278b0dab1f3357e5fc9d24d3a944263a33816b6e
Philip Weir <philip.weir@waivcard.com>
2020-10-06 20:50:26 +0800

    added material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
 function App() {
+
+  const [state, setState] = React.useState({
+    test: false,
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>coding challenge</p>          
+        
       </header>
+      <body>
+        <FormControlLabel 
+            label="test"
+            labelPlacement='start'
+            control={
+              <Switch checked={state.test} 
+                onChange={e => setState({...state, test: e.target.checked})}
+                name="test"
+                color="primary"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+            }/>
+      </body>
     </div>
   );
 }
